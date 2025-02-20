36d59fbbc13dd088cb18b545f8fc449e094eaf81
jrobbl MSI <jrobbl>
2025-02-19 18:00:16 -0600

    initial

---
+++ index.js
@@ -0,0 +1,15 @@
+const { app, BrowserWindow } = require("electron");
+
+let mainWindow;
+
+app.whenReady().then(() => {
+  mainWindow = new BrowserWindow({
+    width: 400,
+    height: 300,
+    webPreferences: {
+      nodeIntegration: true,
+    },
+  });
+
+  mainWindow.loadFile("index.html");
+});
